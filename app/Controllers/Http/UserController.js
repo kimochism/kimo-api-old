88a503b44812307ccefb968b816e2f1b9d02690f
kimochism <kimochism.devs@gmail.com>
2020-09-17 11:42:16 -0300

    feat(heroku): add proc file

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -5,7 +5,7 @@ class UserController {
 
     async store({ request, response }) {
         const data = request.only(['username', 'email', 'password']);
-        
+
         const user = await User.create(data);
         return user;
     }
