0bed7be13633284e3b29e0e1466f65eb6e62bf5e
kimochism <kimochism.devs@gmail.com>
2020-10-03 02:15:46 -0300

    users

--- app/Models/User.js
+++ app/Models/User.js
@@ -1,12 +1,12 @@
 'use strict'
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const BaseModel = use('App/Models/BaseModel')
+const Model = use('Model')
 
 /** @type {import('@adonisjs/framework/src/Hash')} */
 const Hash = use('Hash')
 
-class User extends BaseModel {
+class User extends Model {
 
   static boot() {
     super.boot()
